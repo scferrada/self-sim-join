7812f0043458d5501b3aff1b7eea2a9cf9bf05bd
Sebastián Ferrada <scferradaa@gmail.com>
2017-12-01 19:08:16 -0300

    fixed Lw bug in quickjoin

--- quickjoin/experiment.py
+++ quickjoin/experiment.py
@@ -8,11 +8,12 @@ parser.add_argument('input_matrix', type=str, help='The path to the numpy matrix
 parser.add_argument('output_path', type=str, help='The path where the output must be stored')
 parser.add_argument('--batch_size', dest='batch_size', type=int, default=100, help='The size of the sample incrementation, as a percentage. Default 100')
 parser.add_argument('--N', dest='iter', type=int, default=1, help='The number of times the experiment must be repeated. 1 by default.')
+parser.add_argument('--i', dest='i', type=int, default=9, help='The number of the batch. 9 by default.')
 
 args = parser.parse_args()
 
 DATA_SIZE = 928276
-MAX_THREADS = 20
+MAX_THREADS = 1#0
 
 hashing = {}
 
@@ -25,16 +26,25 @@ for row in data:
 
 def run_t(id):	
 	for j in range(args.iter/MAX_THREADS):
-		for i in range(100/args.batch_size):
-			print("Running %d iteration, %d batch, thread %d" % (j+1, i+1, id))
-			res, distances = qj.quickjoin(data, 35, 10, 10, 0.6)
-			outfile = open(os.path.join(args.outfile, "%d_%d_%d" % (id,i+1,j+1)),'w')
-			for (source, target) in res:
-				txt = "%s; %s\n"
-				source_id = hashing[source.tostring()]
-				target_id = hashing[target.tostring()]
-				outfile.write(txt % (source_id, target_id))
-			outfile.close()
+		#for i in range(100/args.batch_size):
+		print("Running %d iteration, %d batch, thread %d" % (j+1, args.i+1, id))
+		results = {}
+		total_dists = 0
+		until = (DATA_SIZE/10)*(1+args.i)
+		res, distances = qj.quickjoin(data[:until], 0, 10, 10, 0.5)
+		results.update(res)
+		total_dists += distances
+		res, distances = qj.quickjoin(data[:until], 35, 10, 10, 0.5)
+		results.update(res)
+		total_dists += distances
+		outfile = open(os.path.join(args.outfile, "%d_%d_%d" % (id,args.i+1,j+1)),'w')
+		for (source, target) in res:
+			txt = "%s; %s\n"
+			source_id = hashing[source]
+			target_id = hashing[target]
+			outfile.write(txt % (source_id, target_id))
+		outfile.write("%d distances computed" % total_dists)
+		outfile.close()
 
 threads = []
 for i in range(MAX_THREADS):
--- quickjoin/quickjoin.py
+++ quickjoin/quickjoin.py
@@ -11,13 +11,25 @@ def choose_pivots(data):
 	return data[idx[0], :], data[idx[1], :]
 
 def qpartition(data, p, r, rho):
-	dists = np.sum(np.abs(data - p), axis=1))
+	dists = np.sum(np.abs(data - p), axis=1)
 	L = data[dists < rho]
 	G = data[dists >= rho]
 	Gw = data[dists <= (rho + r)]
-	Lw = L[dists >= rho - r]
+	X = dists <= (rho + r)
+	Y = dists >= (rho - r)
+	Lw = L[dists[dists < rho] >= (rho - r)]
 	return L, G, Lw, Gw
 	
+#def bf_join(data1, data2, r, eps):
+#	results = []
+#	distances = 0
+#	for i in range(data1.shape[0]):
+#		for j in range(data2.shape[0]):
+#			if np.sum(np.abs(data1[i]-data2[j])) < (1 - eps) * r:
+#				results.append((data1[i], data2[j]))
+#			distances += 1
+#	return results, distances
+
 def piv_join(data1, data2, r, k, eps):
 	results = []
 	P = []
@@ -36,9 +48,16 @@ def piv_join(data1, data2, r, k, eps):
 				if math.abs(P[l][j] - dists[l]) > (1 - eps) * r:
 					f = True
 					break
-			if not f and np.sum(np.abs(data1[i]-data2[j])) <= r:
+			if not f: #and np.sum(np.abs(data1[i]-data2[j])) <= r: this time we only get the smallest distance per pair.
+				dist  = np.sum(np.abs(data1[i]-data2[j]))
 				distances += 1
-				results.append((data1[i], data2[j]))
+				key = (data1[i].tostring(), data2[j].tostring())
+				if key not in results:
+					results[key] = dist
+					continue
+				min_dist = results[key]
+				if dist < min_dist:
+					results[key] = dist
 	return results, distances
 '''
 Data is a numpy matrix, where every row represents a vector
@@ -54,32 +73,32 @@ def quickjoin(data, r, c, k, eps = 0, distances=0):
 	distances += 1
 	L, G, Lw, Gw = qpartition(data, p1, r, rho)
 	distances += data.shape[0]
-	results = []
-	r, d = quickjoin(L, r, c, k, eps, distances)
-	results.extend(r)
-	r, d = quickjoin(G, r, c, k, eps, d)
-	results.extend(r)
-	r, d = quickjoin_win(Lw, Gw, r, c, k, eps)
-	results.extend(r)
+	results = {}
+	res, d = quickjoin(L, r, c, k, eps, distances)
+	results.update(res)
+	res, d = quickjoin(G, r, c, k, eps, d)
+	results.update(res)
+	res, d = quickjoin_win(Lw, Gw, r, c, k, eps)
+	results.update(res)
 	return results, d
 	
 def quickjoin_win(Lw, Gw, r, c, k, eps, distances):
 	if (Lw.shape[0] + Gw.shape[0]) <= c:
-		res, p_dists piv_join(Lw, Gw, r, k, eps)
+		res, p_dists = piv_join(Lw, Gw, r, k, eps)
 		return res, distances + p_dists
 	p1, p2 = choose_pivots(np.vstack((Lw, Gw)))
 	rho = np.sum(np.abs(p1-p2))
 	distances += 1
 	L1, G1, Lw1, Gw1 = qpartition(Lw, p1, r, rho)
 	L2, G2, Lw2, Gw2 = qpartition(Gw, p1, r, rho)
-	results = []
-	r, d = quickjoin_win(L1, L2, r, c, k, eps, distances)
-	results.extend(r)
-	r, d = quickjoin_win(G1, G2, r, c, k, eps, d)
-	results.extend(r)
-	r, d = quickjoin_win(Lw1, Gw2, r, c, k, eps, d)
-	results.extend(r)
-	r, d = quickjoin_win(Gw1, Lw2, r, c, k, eps, d)
-	results.extend(r)
+	results = {}
+	res, d = quickjoin_win(L1, L2, r, c, k, eps, distances)
+	results.update(res)
+	res, d = quickjoin_win(G1, G2, r, c, k, eps, d)
+	results.update(res)
+	res, d = quickjoin_win(Lw1, Gw2, r, c, k, eps, d)
+	results.update(res)
+	res, d = quickjoin_win(Gw1, Lw2, r, c, k, eps, d)
+	results.update(res)
 	return results, d
 	

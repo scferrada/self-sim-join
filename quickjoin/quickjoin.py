c17ac82b8d7c414ab2058eba9405a8f789bab130
Sebastián Ferrada <scferradaa@gmail.com>
2018-10-09 17:45:45 -0300

    almost there

--- quickjoin/quickjoin.py
+++ quickjoin/quickjoin.py
@@ -9,11 +9,14 @@ by Fredriksson and Braithwaite
 
 class Res:
 	def __init__(self, obj, dist):
-		self.dist = dist
+		self.dist = -dist
 		self.obj = obj
 		
 	def __lt__(self, other):
-		return self.dist > other.dist
+		return self.dist < other.dist
+		
+	def __leq__(self, other):
+		return self.dist <= other.dist
 		
 	def __eq__(self, other):
 		return self.dist == other.dist and self.obj == other.obj
@@ -30,7 +33,7 @@ def qpartition(data, p, r, rho):
 	dists = np.sum(np.abs(data[:,1:] - p[1:]), axis=1)
 	L = data[dists < rho]
 	G = data[dists >= rho]
-	Gw = data[np.logical_and(dists>=rho, dists < (rho + r))]
+	Gw = data[np.logical_and(dists>=rho, dists <= (rho + r))]
 	Lw = data[np.logical_and(dists>=rho-r, dists <rho)]
 	return L, G, Lw, Gw
 
@@ -44,16 +47,18 @@ def piv_join(data1, data2, results, r, k, eps):
 		P.append(np.sum(np.abs(data2[:,1:] - data1[i][1:]), axis = 1))
 		distances += data2.shape[0]
 		for j, dist in enumerate(P[i]):
-			if i==j: continue
+			if data1[i][0]==data2[j][0]: continue
+			if data2[j][0] in [x.obj for x in results[data1[i][0]]]: continue
 			if len(results[data1[i][0]]) < k:
 				heappush(results[data1[i][0]], Res(data2[j][0], dist))
-			elif dist < results[data1[i][0]][0]:
+			elif dist < -results[data1[i][0]][0].dist:
 				heappushpop(results[data1[i][0]], Res(data2[j][0], dist))
 	for i in range(N, data1.shape[0]):
 		dists = np.sum(np.abs(data1[:N, 1:] - data1[i][1:]), axis=1)
 		distances += k
 		for j in range(data2.shape[0]):
-			if i==j: continue
+			if data1[i][0]==data2[j][0]: continue
+			if data2[j][0] in [x.obj for x in results[data1[i][0]]]: continue
 			f = False
 			for l in range(N):
 				if np.abs(P[l][j] - dists[l]) >  r:
@@ -64,9 +69,9 @@ def piv_join(data1, data2, results, r, k, eps):
 				distances += 1
 				if len(results[data1[i][0]]) < k:
 					heappush(results[data1[i][0]], Res(data2[j][0], dist))
-				elif dist < results[data1[i][0]][0]:
+				elif dist < -results[data1[i][0]][0].dist:
 					heappushpop(results[data1[i][0]], Res(data2[j][0], dist))
-	return distances, None
+	return results, distances, None
 	
 def knn_join(data1, data2, results, r, k, eps):
 	d = 0
@@ -75,13 +80,13 @@ def knn_join(data1, data2, results, r, k, eps):
 		distances = np.sum(np.abs(data2[:,1:]-elem_i[1:]), axis=1)
 		d += len(data2)
 		for i, dist in enumerate(distances):
-			if i == elem_i[0]: continue
+			if data2[i][0] == elem_i[0]: continue
 			if dist > maxdist: maxdist = dist
 			if len(results[elem_i[0]]) < k:
-				heappush(results[elem_i[0]], Res(i, dist))
-			elif dist < results[elem_i[0]][0].dist:
-				heappushpop(results[elem_i[0]], Res(i,dist))
-	return d, maxdist
+				heappush(results[elem_i[0]], Res(data2[i][0], dist))
+			elif dist < -results[elem_i[0]][0].dist:
+				heappushpop(results[elem_i[0]], Res(data2[i][0],dist))
+	return results, d, maxdist
 	
 '''
 Data is a numpy matrix, where every row represents a vector
@@ -98,7 +103,7 @@ def quickjoin_iter(data, r, c, k, join_func, results = None, eps = 0, distances=
 	while not q.empty():
 		slice = q.get()
 		if slice.shape[0] <= c:
-			p_dists, maxdist1 = join_func(slice, slice, results, r, k, eps)
+			results, p_dists, maxdist1 = join_func(slice, slice, results, r, k, eps)
 			distances += p_dists
 			q.task_done()
 			continue
@@ -112,16 +117,13 @@ def quickjoin_iter(data, r, c, k, join_func, results = None, eps = 0, distances=
 		q.put(G)
 		if Lw.shape[0] != 0 or Gw.shape[0] != 0:
 			qw.put((Lw, Gw))
-	print("qw")
 	maxdist2 = -1
 	while not qw.empty():
-		print(qw.qsize())
 		Lw, Gw = qw.get()
 		if (Lw.shape[0] + Gw.shape[0]) <= c:
-			p_dists, maxdist2 = join_func(Lw, Gw, results, r, k, eps)
+			results, p_dists, maxdist2 = join_func(Lw, Gw, results, r, k, eps)
 			distances += p_dists
 			qw.task_done()
-			print("cont")
 			continue
 		p1, p2 = choose_pivots(np.vstack((Lw, Gw)))
 		rho = np.sum(np.abs(p1[1:]-p2[1:]))
@@ -143,8 +145,8 @@ def quickjoin(data, k, c):
 	data = np.hstack((idx, data))
 	print("first run of QJ")
 	results, distances, maxdist = quickjoin_iter(data, 0, c, k, knn_join)
-	return results, distances
-	#print("second run of QJ")
-	#results2, distances2, _ = quickjoin_iter(data, maxdist, c, k, piv_join, results)
-	#return results2, distances+distances2
+	#return results, distances
+	print("second run of QJ")
+	results2, distances2, _ = quickjoin_iter(data, maxdist, c, k, piv_join, results)
+	return results2, distances+distances2
 	
--- quickjoin/testQJ.py
+++ quickjoin/testQJ.py
@@ -1,28 +1,27 @@
 import numpy as np
 import quickjoin as qj
 
-N = 100
+N = 1000
 l = []
 for i in range(N):
 	l.append([i,i])
 	
 data = np.array(l)
 
-res = qj.quickjoin(data, 2, 4)
+res, _ = qj.quickjoin(data, 2, 10)
 
-print len(res)
-print len(res[0])
-print res[1]
+#for i,x in enumerate(res):
+	#print i, res[x][0].obj, res[x][1].obj
 
-print [x.obj for x in res[0][30]]
+assert 1 in [x.obj for x in res[0]]
+assert 2 in [x.obj for x in res[0]]
+assert N-2 in [x.obj for x in res[N-1]]
+assert N-3 in [x.obj for x in res[N-1]]
 
-#assert 2 in [x.obj for x in res[0]]
-#assert 3 in [x.obj for x in res[0]]
-#assert N-1 in [x.obj for x in res[N]]
-#assert N-2 in [x.obj for x in res[N]]
-
-#for i in range(1,N-1):
-#	r = [res[i][x].obj for x in res[i]]
-#	assert i-1 in r
-#	assert i+1 in r
+for i in range(1,N-1):
+	print i
+	r = [x.obj for x in res[i]]
+	print r
+	assert i-1 in r
+	assert i+1 in r
 	
